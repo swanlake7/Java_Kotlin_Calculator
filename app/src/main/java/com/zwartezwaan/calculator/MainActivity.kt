4ef32c13b66d52a47d003f1c0f184059d11bfd02
Pearl Jean <perlegirl@protonmail.com>
2020-04-22 16:02:35 -0400

    added button functions, changed button designs and added margin

--- app/src/main/java/com/zwartezwaan/calculator/MainActivity.kt
+++ app/src/main/java/com/zwartezwaan/calculator/MainActivity.kt
@@ -13,9 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+var op = "*"
+var oldNum = ""
+var isNewOp = true
+
     fun buEvnt(view:View){
-        var data = viewText.text
+        if(isNewOp == true){
+            viewText.setText("")
+        }
+        isNewOp = false
+        //sel = clicked button
         var sel = view as Button
+        //clicked= what's displayed on screen
         var clicked:String = viewText.text.toString()
         when(sel.id){
             bu0.id -> {clicked +="0"}
@@ -28,10 +37,47 @@ class MainActivity : AppCompatActivity() {
             bu7.id -> {clicked +="7"}
             bu8.id -> {clicked +="8"}
             bu9.id -> {clicked +="9"}
+
             //try to prevent adding more than one dot
             buDot.id -> {clicked +="."}
             buPlMi.id -> {clicked ="-" + clicked}
         }
         viewText.setText(clicked)
     }
+
+    fun buOpEv(view:View){
+        val sel = view as Button
+        when(sel.id){
+            buDiv.id ->{ op = "/"}
+            buMult.id->{  op = "*"}
+            buMinus.id -> { op = "-"}
+            buPlus.id -> { op = "+"}
+        }
+        oldNum = viewText.text.toString()
+        isNewOp = true
+    }
+
+    fun buEqlEvnt(view:View){
+        var newNum = viewText.text.toString()
+        var finalNum:Double?=null
+        when(op){
+            "*" ->{finalNum = oldNum.toDouble() * newNum.toDouble()}
+            "/" ->{finalNum = oldNum.toDouble() / newNum.toDouble()}
+            "-" ->{finalNum = oldNum.toDouble() - newNum.toDouble()}
+            "+" ->{finalNum = oldNum.toDouble() + newNum.toDouble()}
+        }
+        viewText.setText(finalNum.toString())
+        isNewOp = true
+    }
+
+    fun buPercEvnt(view:View){
+        var num:Double = viewText.text.toString().toDouble()/100
+        viewText.setText(num.toString())
+    }
+
+    fun buCleanEvnt(view: View){
+        viewText.setText("")
+        isNewOp= true
+    }
+
 }
